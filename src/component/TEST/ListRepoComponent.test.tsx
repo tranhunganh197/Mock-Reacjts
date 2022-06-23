7a60fff0ca0bee6120dd767c675b16e48fe630e2
anhth34 <tranhunganh197@gmail.com>
2022-06-23 14:24:24 +0700

    not done jest

--- src/component/TEST/ListRepoComponent.test.tsx
+++ src/component/TEST/ListRepoComponent.test.tsx
@@ -1,6 +1,10 @@
+import React from 'react';
 import ListRepoComponent from 'component/list-repo/list-repo';
 import configureStore from 'redux-mock-store';
 import ReactTestUtils from 'react-dom/test-utils'; 
+import { mount } from 'enzyme'; 
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux'
 import { useSelector, useDispatch } from 'react-redux'; 
 
 const mockDispatch = jest.fn();
@@ -8,6 +12,11 @@ jest.mock('react-redux', () => ({
   useSelector: jest.fn(),
   useDispatch: () => mockDispatch
 }));
+// const mockDispatch = jest.fn();
+// jest.mock('react-redux', () => ({
+//   useSelector: jest.fn(),
+//   useDispatch: () => mockDispatch
+// }));
 
 // it('loads data on init', () => {
 //   const mockedDispatch = jest.fn();
@@ -20,21 +29,27 @@ jest.mock('react-redux', () => ({
 
 describe('With React Testing Library', () => {
     const initialState = { output: '' };
+describe('<ListRepoComponent />', () => {
+  it('has a div tag', () => {
     const mockStore = configureStore();
     let store;
 
     it('has store', () => {
         store = mockStore(initialState);
         expect(store).not.toBeNull();
+    const store = mockStore({
+      repo: {
+        repo: []
+      }
     });
 })
 
 describe('ListRepoComponent', () => {
- 
+
     it('passing test', () => {
       expect(true).toBeTruthy();
     })
-   
+
     it('has a div tag', () => {
         const component:any = ReactTestUtils.renderIntoDocument(<ListRepoComponent/>);
         let div = ReactTestUtils.findRenderedDOMComponentWithTag(
@@ -49,5 +64,16 @@ describe('ListRepoComponent', () => {
         );
     })
   })
-//
-export {};
+
+export {}; 
+    const wrapper = mount(
+      <Provider store={store}>
+        <BrowserRouter>
+          <ListRepoComponent />
+        </BrowserRouter>
+      </Provider>
+    );
+
+    expect(wrapper.find('a').text()).toEqual('ADD ')
+  });
+});
